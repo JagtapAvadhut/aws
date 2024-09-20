f8040adb66c52f05f81e5d966c3f9b37f296b0d0
Avadhut <avadhutjagtap9999@gmail.com>
2024-09-20 16:39:30 +0530

    inside main class added postman curl

--- src/main/java/com/aws/AwsApplication.java
+++ src/main/java/com/aws/AwsApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AwsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AwsApplication.class, args);
+    }
+//curl --location 'localhost:8080/users'
+//	curl --location 'localhost:8080/users/faf4d83e-c1d2-4831-a57b-58fe733f8601'
 }
